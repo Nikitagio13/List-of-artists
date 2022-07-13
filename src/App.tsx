68add0e5d3d7756f3eade390ee4a281e96f1ce7c
Nikitagio13 <nikitagio13@gmail.com>
2022-07-13 10:55:27 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import List from "./components/List";
+
+
+interface IState {
+  people: {
+      name: string
+      age: number
+      url: string
+      note?: string
+  }[];
+}
+
 
 function App() {
+
+const [people, setPeople] = useState<IState["people"]>( [
+    {
+      name: "Giko Megrelishvili",
+      url: "https://scontent.ftbs5-3.fna.fbcdn.net/v/t1.6435-9/120013139_4401889183216711_4907294256895663160_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_ohc=NZM83K0AA_QAX8HGSyE&tn=fnGUHrQXfJJ9b15H&_nc_ht=scontent.ftbs5-3.fna&oh=00_AT-Dn8NmVrNx8no3gUdScHLoJ8vYoxZE8BjOVzdflzbC0A&oe=62E20CED",
+      age: 24,
+      note: "Co-Founder of  'Cypher' "
+    }
+] )
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>People invited to Cypher's party 2022</h1>
+        <List people={people}/>
     </div>
   );
 }
